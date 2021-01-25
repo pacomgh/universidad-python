74ae8c0c35044f16352f29c01f8ea0d26eb51528
Francisco Guzmán <paco.mgh@gmail.com>
2021-01-25 16:04:22 -0600

    add logger

---
+++ 026-capa_datos_persona/conexion.py
@@ -0,0 +1,4 @@
+from logger_base import logger
+
+if __name__ == '__main__':
+    logger.info('Desde conexion')
--- 026-capa_datos_persona/logger_base.py
+++ 026-capa_datos_persona/logger_base.py
@@ -1,16 +1,27 @@
 #logging nos permitira enviar informacion a multiples destinos, incluida la consola
 #podemos priorizar los mensajes
 import logging
-
+#variable logger a utilizar
 logger = logging
 
 ##definimos el nivel desde el que deseamos enviar mensajes
 #debug solo se usa en etapa de desarrollo
-logger.basicConfig(level=logging.WARNING)
+#handlers son los diferentes medios por los cuales enviamos informacion
+logger.basicConfig(level=logging.DEBUG,#nivel debug hacia arriba
+                   #modifica el formato de la informacion que se envia
+                   format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                   datefmt='%I:%M:%S %p',
+                   handlers=[
+                       logging.FileHandler('capa_datos.log'),#archivo que va a recibir la informacion
+                       logging.StreamHandler(),#se envia la informacion a la consola
+                   ])
 
-logging.warning('Mensaje a nivel warning')
-logging.info('Mensaje a nivel info')
-logging.debug('Mensaje a nivel debug')
+#asignamos el nombre main a este archivo
+if __name__ == '__main__':
+    #solo se imprime esta informacion cuando se ejecuta este archivo
+    logging.warning('Mensaje a nivel warning')
+    logging.info('Mensaje a nivel info')
+    logging.debug('Mensaje a nivel debug')
 
-logging.error('Ocurrio un error en la base de datos')
-logging.debug('Se realizo la conexion con éxito')
+    logging.error('Ocurrio un error en la base de datos')
+    logging.debug('Se realizo la conexion con éxito')
