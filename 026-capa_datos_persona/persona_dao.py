1d9810a20c2f35536caefa069e96858f55d37c36
Francisco Guzmán <paco.mgh@gmail.com>
2021-04-22 17:29:41 -0500

    add insert method and SQL

--- 026-capa_datos_persona/persona_dao.py
+++ 026-capa_datos_persona/persona_dao.py
@@ -8,6 +8,7 @@ class PersonaDao:
     sobre la entidad persona
     '''
     __SELECCIONAR = 'SELECT  * from persona ORDER BY id_persona'
+    __INSERTAR = 'INSERT INTO persona(nombre,apellido,email) VALUES(%s, %s, %s)'
     
     @classmethod
     def seleccionar(cls):
@@ -26,13 +27,37 @@ class PersonaDao:
             persona = Persona(registro[0], registro[1], registro[2], registro[3])
             #agregamos al arreglo cada objeto persona
             personas.append(persona)
+        Conexion.cerrar()
         return personas
+    
+    @classmethod
+    #mandamos los parametros en el objeto eprsona
+    def insertar(cls, persona):
+        try:
+            conexion = Conexion.obtenerConexion()
+            cursor = Conexion.obtenerCursor()
+            logger.debug(cursor.mogrify(cls.__INSERTAR))       
+            logger.debug(f'Persona a insrtar: {persona}')       
+            #creamos tupla de valores
+            valores = (persona.getNombre(), persona.getApellido(), persona.getEmail())
+            cursor.execute(cls.__INSERTAR, valores)
+            conexion.commit()#mandamos a base de datos            
+            return cursor.rowcount()#indica cuantos registros se insertaron
+        except Exception as e:
+            conexion.rollback()#si algo sale mal damos marcha atras
+            logger.error(f'Excepcion, error al insertar persona: {e}')   
+        finally:
+            Conexion.cerrar()
     
 
 if __name__ == '__main__':
-    personas = PersonaDao.seleccionar()
-    for persona in personas:
-        logger.debug(persona)
-        logger.debug(persona.getIdPersona())
-            
-        
+    #personas = PersonaDao.seleccionar()
+    #for persona in personas:
+    #    logger.debug(persona)
+    #    logger.debug(persona.getIdPersona())
+    
+    #inseramos nuevo registro
+    persona=Persona(nombre='Pedro', apellido='Najera', email='pnajera@mail.com')
+    personas_insertadas=PersonaDao.insertar(persona)
+    
+    logger.debug(f'Registros insertados: {personas_insertadas}')
